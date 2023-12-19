68162b3b07a06a68cb8fb75eb56e78717a2e9ded
Leonardo Fonteles <fonteles.leonardo@gmail.com>
2023-12-19 13:37:15 -0300

    iniciando projeto de site-especial

---
+++ script.js
@@ -0,0 +1,17 @@
+function mostrarResposta() {
+
+    window.alert('Que bom que escolheu SIM, não tinha escolha mesmo kkkk');
+    window.alert('Agora vamos para outro parte do site');
+    window.alert('Uiiiiii');
+    window.location.href = './pagina2/pagina2.html'
+
+
+}
+
+function mudarPosicao() {
+    const buttonNao = document.getElementById('nao');
+    const randomX = Math.floor(Math.random() * window.innerWidth);
+    const randomY = Math.floor(Math.random() * window.innerHeight);
+    buttonNao.style.left = `${randomX}px`;
+    buttonNao.style.top = `${randomY}px`
+}
